e59e7619c7be16563e8670149fe45c8f16a959af
Aldair <adrianaaylinmaldonadosiles@gmail.com>
2026-09-08 23:21:14 -0400

    Actualizar invitación Baby Shower

--- app/page.tsx
+++ app/page.tsx
@@ -335,7 +335,7 @@ export default function Home() {
 
       <audio
         ref={audioRef}
-        src="/musica/musica2.mp3"
+        src="/musica/musica3.mp3"
         loop
         preload="auto"
       />
